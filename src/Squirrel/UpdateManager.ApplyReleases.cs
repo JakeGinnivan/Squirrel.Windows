a709b82c13ab68b8511229a6b571b3334f6bc946
Jake Ginnivan <jake@ginnivan.net>
2016-01-11 08:34:17 +0800

    Fixed spacing

--- src/Squirrel/UpdateManager.ApplyReleases.cs
+++ src/Squirrel/UpdateManager.ApplyReleases.cs
@@ -539,12 +539,12 @@ namespace Squirrel
                 new[] { RegistryView.Registry32, RegistryView.Registry64 }.ForEach(view => {
                     var baseKey = default(RegistryKey);
                     var regKey = default(RegistryKey);
-		            var layersKey = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers";
+                    var layersKey = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers";
 
                     try {
                         baseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, view);
-            			regKey = baseKey.OpenSubKey(layersKey) ??
-            					 baseKey.CreateSubKey(layersKey);
+                        regKey = baseKey.OpenSubKey(layersKey) ??
+                                 baseKey.CreateSubKey(layersKey);
 
                         var toDelete = regKey.GetValueNames()
                             .Where(x => x.StartsWith(rootAppDirectory, StringComparison.OrdinalIgnoreCase))
